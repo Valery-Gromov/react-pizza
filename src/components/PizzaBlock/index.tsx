4292785cd0c78d039bd5161bd5783578dc9f9f2b
Valerii Gromov <kar.tell@yandex.ru>
2024-02-08 14:53:49 +0200

    hotfixes

--- src/components/PizzaBlock/index.tsx
+++ src/components/PizzaBlock/index.tsx
@@ -68,7 +68,7 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({ id, title, price, imageUrl, typ
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">{`from ${price} ₽`}</div>
+        <div className="pizza-block__price">{`From ${price} ₽`}</div>
         <div onClick={onClickAdd} className="button button--outline button--add">
           <svg
             width="12"
